81dc4667c615e6272a112a259f5faa12671145f6
Chetana Hanmant Nirmal <chn3060@gmail.com>
2021-12-06 21:21:26 -0500

    Update User in the database

--- data/updateProfile.js
+++ data/updateProfile.js
@@ -1,6 +1,7 @@
 const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
 let { ObjectId } = require('mongodb');
+const bcrypt = require('bcrypt');
 
 async function getRegisteredUser(user){
 try {
@@ -9,7 +10,7 @@ try {
     const collection = await users();
     const res = await collection.findOne( {email: user});
     if(!res){
-        throw `Either the username or password is invalid!`;
+        throw `User not found`;
     }else{
         return res;
     }
@@ -20,7 +21,64 @@ try {
     }
 }
 
+async function checkPassword(response, age, phoneNumber, houseNumber, street
+    ,city,state,zip){
+
+     
+    // if(!username || (response.email !== username)){
+    // username = username.trim();
+    // username = username.toLowerCase();
+    //     throw `Can not change username`;
+    // }
+    
+    // if(!password){
+    //     throw `Cannot modify password`;
+    // }
+
+    // password = password.trim();
+    
+    // const match = await bcrypt.compare(password, response.password);
+    // if(!match){
+    //     throw `Cannot modify password`;
+    // }
+
+    // if(!firstName || (response.firstName !== username)){
+    //     throw `Cannot modify First Name`;
+    // }
+
+    // if(!lastName || (response.lastName !== lastName)){
+    //     throw `Cannot modify First Name`;
+    // }
+
+    const dataToUpdate = {};
+
+    dataToUpdate['age'] = age;
+    dataToUpdate['phoneNumber'] = phoneNumber;
+
+    const address = {};
+    address['number'] = houseNumber;
+    address["street"] = street;
+    address["city"] = city;
+    address["state"] = state;
+    address["zip"] = zip;
+    dataToUpdate["address"] = address;
+
+
+    const userCollection = await users();
+
+    var updateUser = await userCollection.updateOne({ _id:  response._id},
+       { $set:  dataToUpdate });
+    
+    console.log("user updated from data!");
+
+    if (updateUser.insertedCount === 0){
+        throw `Could not update user`;
+    }  
+
+    return {userUpdated: true};
+}
 
     module.exports = {
-        getRegisteredUser
+        getRegisteredUser,
+        checkPassword
     };
--- routes/updateProfile.js
+++ routes/updateProfile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const data = require('../data');
 const updateprofiledata = data.updateprofiledata;
 const e = require('express');
+const xss = require('xss');
 
 router.get('/', async(req, res) => {
     let errors = [];
@@ -19,8 +20,83 @@ router.get('/', async(req, res) => {
 });
 
 
-router.post('/:id', async(req, res) => {
-    
+router.post('/', async(req, res) => {
+
+    let user = req.session.user;
+
+    try {
+        let email = xss(req.body.email).toLowerCase();
+        let password = xss(req.body.password).toLowerCase();
+        let firstName = xss(req.body.firstName).toLowerCase();
+        let lastName = xss(req.body.lastName).toLowerCase();
+        let age = xss(req.body.age).toLowerCase();
+        let phoneNumber = xss(req.body.phoneNumber).toLowerCase();
+        let houseNumber = xss(req.body.houseNumber).toLowerCase();
+        let street = xss(req.body.street).toLowerCase();
+        let city = xss(req.body.city).toLowerCase();
+        let state = xss(req.body.state).toLowerCase();
+        let zip = xss(req.body.zip).toLowerCase();
+        
+        if(email){
+            throw `Cannot Update Username`;
+        }
+        if(password){
+            throw `Cannot Update Password`;
+        }
+        if(firstName){
+            throw `Cannot Update Firstname`;
+        }
+        if(lastName){
+            throw `Cannot Update Lastname`;
+        }
+
+        //Required Fields
+        if(!age){
+            throw `Age is required!`;
+        }
+        if(!phoneNumber){
+            throw `Phone Number is required!`;
+        }
+        if(!houseNumber){
+            throw `House Number is required!`;
+        }
+        if(!street){
+            throw `Street is required`;
+        }
+        if(!city){
+            throw `City is required!`;
+        }
+        if(!state){
+            throw `State is required!`;
+        }
+        if(!zip){
+            throw `Zip is required!`;
+        }
+       
+        age = parseInt(age);
+        if(age < 18){
+            throw `You're below 18, sorry!`;
+        }
+
+        if(phoneNumber.length < 10){
+            throw `Invalid phone number!`;
+        }else if(!/^\d+$/.test(phoneNumber)){
+            throw 'Invalid phone number!';
+        }
+
+        const response = await updateprofiledata.getRegisteredUser(user);
+
+        const resp = await updateprofiledata.checkPassword(response, age, phoneNumber, houseNumber, street
+            ,city,state,zip);
+
+        if(resp.userUpdated){
+            res.status(200).render('login/updateProfile.handlebars', { loginUser: true,user: user ,role : req.session.role , error : "User Updated Successfully"});
+        }
+
+        // return res.status(500).render('login/error', {error:"Internal Server Error!"});
+    } catch (error) {
+        res.status(400).render('login/updateProfile.handlebars', {error:error});
+    }    
 });
 
 module.exports = router;
