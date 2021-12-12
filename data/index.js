bff1d58b8a763c740cf3089bb3ab6e8850321979
Anshul Mistry <amm.suthar@gmail.com>
2021-12-12 18:37:14 -0500

    make it better

--- data/index.js
+++ data/index.js
@@ -2,13 +2,13 @@ const usersData = require('./users');
 const searchCarData = require("./searchCar");
 const approveCarsData = require("./approveCars");
 const updateProfileData = require("./updateProfile");
-const userProfile = require("./userprofile");
+const userProfile = require("./userProfile");
 
 
 module.exports = {
     usersdata: usersData,
-    searchcardata : searchCarData,
-    approvecarsdata : approveCarsData,
-    updateprofiledata : updateProfileData,
-    userprofile : userProfile
+    searchcardata: searchCarData,
+    approvecarsdata: approveCarsData,
+    updateprofiledata: updateProfileData,
+    userprofile: userProfile
 };
